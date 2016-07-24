914e8c12344d09ee509f451f62f982cb66c35487
Alan Cao <alannesta@sina.com>
2016-07-24 16:56:50 -0400

    logger level

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,7 @@ var winston = require('winston');
 var logger = new (winston.Logger)({
 	transports: [
 		new (winston.transports.Console)({
-			level: 'debug'
+			level: process.env.NODE_ENV === 'production'? 'info' : 'debug'
 		})
 		//new (winston.transports.File)({ filename: '/Users/sijin.cao/Desktop/winstonlog.log' })
 	]
